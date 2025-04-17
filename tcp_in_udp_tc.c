228280cf4d23017aab86e30d12d72f5bd21774f0
Matthieu Baerts <matttbe@kernel.org>
2025-04-17 18:51:23 +0200

    WIP
    Signed-off-by: Matthieu Baerts (NGI0) <matttbe@kernel.org>

---
+++ tcp_in_udp_tc.c
@@ -0,0 +1,331 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#include <linux/bpf.h>
+#include <linux/in.h>
+
+#include <stddef.h>
+#include <linux/if_ether.h>
+#include <linux/if_packet.h>
+#include <linux/ip.h>
+#include <linux/ipv6.h>
+#include <linux/tcp.h>
+#include <linux/udp.h>
+#include <linux/pkt_cls.h>
+#include <bpf/bpf_helpers.h>
+#include <bpf/bpf_endian.h>
+
+
+struct tcp_in_udp_hdr {
+	struct udphdr udphdr;
+	__be32	doff_flags_window;
+	__be32	seq;
+	__be32	ack_seq;
+};
+
+/* Header cursor to keep track of current parsing position */
+struct hdr_cursor {
+	void *pos;
+};
+
+/*******************************************
+ ** parse_*hdr helpers from XDP tutorials **
+ *******************************************/
+
+/* @return: next header */
+static __always_inline int parse_ethhdr(struct hdr_cursor *nh,
+					void *data_end,
+					struct ethhdr **ethhdr)
+{
+	struct ethhdr *eth = nh->pos;
+	int hdrsize = sizeof(*eth);
+
+	/* Byte-count bounds check; check if current pointer + size of header
+	 * is after data_end.
+	 */
+	if ((void *)eth + hdrsize > data_end)
+		return -1;
+
+	nh->pos += hdrsize;
+	*ethhdr = eth;
+
+	return eth->h_proto; /* network-byte-order */
+}
+
+/* @return: next header */
+static __always_inline int parse_ip6hdr(struct hdr_cursor *nh,
+					void *data_end,
+					struct ipv6hdr **ip6hdr)
+{
+	struct ipv6hdr *ip6h = nh->pos;
+	int hdrsize = sizeof(*ip6h);
+
+	if ((void *)ip6h + hdrsize > data_end)
+		return -1;
+
+	nh->pos += hdrsize;
+	*ip6hdr = ip6h;
+
+	/* TODO: support extensions */
+	return ip6h->nexthdr;
+}
+
+/* @return: next header */
+static __always_inline int parse_iphdr(struct hdr_cursor *nh,
+				       void *data_end,
+				       struct iphdr **iphdr)
+{
+	struct iphdr *iph = nh->pos;
+	int hdrsize = sizeof(*iph);
+
+	if ((void *)iph + hdrsize > data_end)
+		return -1;
+
+	hdrsize = iph->ihl << 2;
+	/* Sanity check packet field is valid */
+	if(hdrsize < sizeof(*iph))
+		return -1;
+
+	/* Variable-length IPv4 header, need to use byte-based arithmetic */
+	if (nh->pos + hdrsize > data_end)
+		return -1;
+
+	nh->pos += hdrsize;
+	*iphdr = iph;
+
+	return iph->protocol;
+}
+
+/* @return: header len */
+static __always_inline int parse_tcphdr(struct hdr_cursor *nh,
+					void *data_end,
+					struct tcphdr **tcphdr)
+{
+	struct tcphdr *tcph = nh->pos;
+	int hdrsize = sizeof(*tcph);
+
+	if ((void *)tcph + hdrsize > data_end)
+		return -1;
+
+	hdrsize = tcph->doff << 2;
+	/* Sanity check packet field is valid */
+	if(hdrsize < sizeof(*tcph))
+		return -1;
+
+	/* Variable-length TCP header, need to use byte-based arithmetic */
+	if (nh->pos + hdrsize > data_end)
+		return -1;
+
+	nh->pos += hdrsize;
+	*tcphdr = tcph;
+
+	return hdrsize;
+}
+
+/* @return: payload len */
+static __always_inline int parse_udphdr(struct hdr_cursor *nh,
+					void *data_end,
+					struct udphdr **udphdr)
+{
+	struct udphdr *udph = nh->pos;
+	int hdrsize = sizeof(*udph);
+	int len;
+
+	if ((void *)udph + hdrsize > data_end)
+		return -1;
+
+	nh->pos += hdrsize;
+	*udphdr = udph;
+
+	len = bpf_ntohs(udph->len) - hdrsize;
+	if (len < 0)
+		return -1;
+
+	return len;
+}
+
+
+/*************
+ ** Ingress **
+ *************/
+
+static __always_inline void udp_to_tcp(struct __sk_buff *skb,
+				       struct hdr_cursor *nh, void *data,
+				       void *data_end, struct iphdr *iphdr,
+				       struct ipv6hdr *ipv6hdr)
+{
+	struct tcp_in_udp_hdr *tuhdr, tuhdr_cpy;
+	struct tcphdr *tcphdr = nh->pos;
+	int nh_off = nh->pos - data;
+	__be16 proto_old, proto_new;
+
+	if (parse_udphdr(nh, data_end, (struct udphdr**)&tuhdr) < 0 ||
+	    (void *)tuhdr + sizeof(struct tcphdr) > data_end)
+		goto out;
+
+	/* Change protocol: UDP -> TCP */
+	if (iphdr)
+		iphdr->protocol = IPPROTO_TCP;
+	else if (ipv6hdr)
+		ipv6hdr->nexthdr = IPPROTO_TCP;
+	else
+		goto out;
+
+	proto_old = bpf_htons(IPPROTO_UDP);
+	proto_new = bpf_htons(IPPROTO_TCP);
+
+	/* Do the modification before calling bpf_...(skb) helpers which can
+	 * modify the SKB and cause "invalid mem access 'scalar'" errors.
+	 */
+	__builtin_memcpy(&tuhdr_cpy, tuhdr, sizeof(struct tcphdr));
+	tcphdr->seq = tuhdr_cpy.seq;
+	tcphdr->ack_seq = tuhdr_cpy.ack_seq;
+	__builtin_memcpy((void *)tcphdr + sizeof(__be32) * 3,
+			 &tuhdr_cpy.doff_flags_window, sizeof(__be32));
+	tcphdr->check = tuhdr_cpy.udphdr.check;
+	tcphdr->urg_ptr = 0;
+
+	bpf_l4_csum_replace(skb, nh_off + offsetof(struct tcphdr, check),
+			    proto_old, proto_new,
+			    BPF_F_PSEUDO_HDR | sizeof(__be16));
+	bpf_l4_csum_replace(skb, nh_off + offsetof(struct tcphdr, check),
+			    tuhdr_cpy.udphdr.len, 0,
+			    BPF_F_PSEUDO_HDR | sizeof(__be16));
+
+	if (iphdr) {
+		bpf_l3_csum_replace(skb, ((void*)iphdr - data) +
+					  offsetof(struct iphdr, check),
+					  proto_old, proto_new, sizeof(__be16));
+	}
+out:
+	return;
+}
+
+SEC("tc_ingress")
+int tcp_ingress_drop(struct __sk_buff *skb)
+{
+	void *data_end = (void *)(long)skb->data_end;
+	void *data = (void *)(long)skb->data;
+	struct hdr_cursor nh = { .pos = data };
+	int eth_type, ip_type, ret = TC_ACT_OK;
+	struct ipv6hdr *ipv6hdr = NULL;
+	struct iphdr *iphdr = NULL;
+	struct ethhdr *eth;
+
+	eth_type = parse_ethhdr(&nh, data_end, &eth);
+	if (eth_type == bpf_htons(ETH_P_IP)) {
+		ip_type = parse_iphdr(&nh, data_end, &iphdr);
+	} else if (eth_type == bpf_htons(ETH_P_IPV6)) {
+		ip_type = parse_ip6hdr(&nh, data_end, &ipv6hdr);
+	} else {
+		goto out;
+	}
+
+	if (ip_type == IPPROTO_UDP)
+		udp_to_tcp(skb, &nh, data, data_end, iphdr, ipv6hdr);
+
+out:
+	return ret;
+}
+
+
+/************
+ ** Egress **
+ ************/
+
+static __always_inline void tcp_to_udp(struct __sk_buff *skb,
+				       struct hdr_cursor *nh, void *data,
+				       void *data_end, struct iphdr *iphdr,
+				       struct ipv6hdr *ipv6hdr)
+{
+	struct tcp_in_udp_hdr *tuhdr = nh->pos;
+	struct tcphdr *tcphdr, tcphdr_cpy;
+	int nh_off = nh->pos - data;
+	__be16 proto_old, proto_new;
+	__be16 udp_len;
+
+	if (parse_tcphdr(nh, data_end, &tcphdr) < 0)
+		goto out;
+
+	if (tcphdr->urg) {
+		if (iphdr)
+			bpf_printk("Skip: %pI4:%u -> %pI4:%u: urgent\n",
+				   bpf_ntohl(iphdr->saddr),
+				   bpf_ntohs(tcphdr->source),
+				   bpf_ntohl(iphdr->daddr),
+				   bpf_ntohs(tcphdr->dest));
+		else if (ipv6hdr)
+			bpf_printk("Skip: %pI6c:%u -> %pI6c:%u: urgent\n",
+				   &ipv6hdr->saddr,
+				   bpf_ntohs(tcphdr->source),
+				   &ipv6hdr->daddr,
+				   bpf_ntohs(tcphdr->dest));
+		goto out; /* TODO: or set to 0 and adapt checksum? */
+	}
+
+	/* Change protocol: TCP -> UDP */
+	if (iphdr) {
+		iphdr->protocol = IPPROTO_UDP;
+		udp_len = bpf_htons(bpf_ntohs(iphdr->tot_len) -
+				    ((void*)tcphdr - (void*)iphdr));
+	} else if (ipv6hdr) {
+		ipv6hdr->nexthdr = IPPROTO_UDP;
+		udp_len = ipv6hdr->payload_len;
+	} else {
+		goto out;
+	}
+
+	proto_old = bpf_htons(IPPROTO_TCP);
+	proto_new = bpf_htons(IPPROTO_UDP);
+
+	/* Do the modification before calling bpf_...(skb) helpers which can
+	 * modify the SKB and cause "invalid mem access 'scalar'" errors.
+	 */
+	__builtin_memcpy(&tcphdr_cpy, tcphdr, sizeof(struct tcphdr));
+	tuhdr->udphdr.len = udp_len;
+	tuhdr->udphdr.check = tcphdr_cpy.check;
+	__builtin_memcpy(&tuhdr->doff_flags_window, &tcphdr_cpy + 12, sizeof(__be32));
+	tuhdr->seq = tcphdr_cpy.seq;
+	tuhdr->ack_seq = tcphdr_cpy.ack_seq;
+
+	bpf_l4_csum_replace(skb, nh_off + offsetof(struct udphdr, check),
+			    proto_old, proto_new,
+			    BPF_F_PSEUDO_HDR | sizeof(__be16));
+	bpf_l4_csum_replace(skb, nh_off + offsetof(struct udphdr, check),
+			    0, udp_len, BPF_F_PSEUDO_HDR | sizeof(__be16));
+
+	if (iphdr) {
+		bpf_l3_csum_replace(skb, ((void*)iphdr - data) +
+					  offsetof(struct iphdr, check),
+					  proto_old, proto_new, sizeof(__be16));
+	}
+out:
+	return;
+}
+
+SEC("tc_egress")
+int tcp_egress_ack(struct __sk_buff *skb)
+{
+	void *data_end = (void *)(long)skb->data_end;
+	void *data = (void *)(long)skb->data;
+	struct hdr_cursor nh = { .pos = data };
+	int eth_type, ip_type, ret = TC_ACT_OK;
+	struct ipv6hdr *ipv6hdr = NULL;
+	struct iphdr *iphdr = NULL;
+	struct ethhdr *eth;
+
+	eth_type = parse_ethhdr(&nh, data_end, &eth);
+	if (eth_type == bpf_htons(ETH_P_IP)) {
+		ip_type = parse_iphdr(&nh, data_end, &iphdr);
+	} else if (eth_type == bpf_htons(ETH_P_IPV6)) {
+		ip_type = parse_ip6hdr(&nh, data_end, &ipv6hdr);
+	} else {
+		goto out;
+	}
+
+	if (ip_type == IPPROTO_TCP)
+		tcp_to_udp(skb, &nh, data, data_end, iphdr, ipv6hdr);
+
+out:
+	return ret;
+}
+char _license[] SEC("license") = "GPL";
+
---
+++ test.sh
@@ -0,0 +1,115 @@
+#!/bin/bash -e
+# SPDX-License-Identifier: GPL-2.0
+# Copyright (c) 2025, Matthieu Baerts.
+
+export NS=tcp
+export HOSTS=(cli cpe net srv)
+
+netns()
+{
+	local suffix
+	local nss=()
+	for suffix in "${HOSTS[@]}"; do
+		nss+=("${NS}_${suffix}")
+	done
+	echo "${nss[@]}"
+}
+
+cleanup()
+{
+	local suffix
+	for suffix in "${HOSTS[@]}"; do
+		local ns="${NS}_${suffix}"
+		ip netns pids "${ns}" | xargs -r kill
+		ip netns del "${ns}" >/dev/null 2>&1
+	done
+}
+
+trap cleanup EXIT
+
+server()
+{
+	ip netns exec "${NS}_srv" iperf3 -s -D
+	sleep .1 # making sure the daemon is launched
+}
+
+tc_client()
+{
+	local ns="${NS}_cpe" iface="net"
+
+	ip netns exec "${ns}" mount -t debugfs none /sys/kernel/debug
+
+	tc -n "${ns}" qdisc add dev "${iface}" clsact
+	tc -n "${ns}" filter add dev "${iface}" egress  bpf da obj tcp_in_udp_tc.o sec tc_egress
+	tc -n "${ns}" filter add dev "${iface}" ingress bpf da obj tcp_in_udp_tc.o sec tc_ingress
+
+	tc -n "${ns}" filter show dev "${iface}" egress
+	tc -n "${ns}" filter show dev "${iface}" ingress
+}
+
+tc_server()
+{
+	local ns="${NS}_srv" iface="net"
+
+	ip netns exec "${ns}" mount -t debugfs none /sys/kernel/debug
+
+	tc -n "${ns}" qdisc add dev "${iface}" clsact
+	tc -n "${ns}" filter add dev "${iface}" egress  bpf da obj tcp_in_udp_tc.o sec tc_egress
+	tc -n "${ns}" filter add dev "${iface}" ingress bpf da obj tcp_in_udp_tc.o sec tc_ingress
+
+	tc -n "${ns}" filter show dev "${iface}" egress
+	tc -n "${ns}" filter show dev "${iface}" ingress
+}
+
+setup()
+{
+	local suffix
+	for suffix in "${HOSTS[@]}"; do
+		local ns="${NS}_${suffix}"
+		ip netns add "${ns}"
+		ip -n "${ns}" link set lo up
+	done
+
+	#        .0.2  .0.1   .1.2  .1.1   .2.1  .2.2
+	#     cli -------- cpe -------- net -------- srv
+
+	ip link add "cli" netns "${NS}_cpe" type veth peer name "cpe" netns "${NS}_cli"
+	ip link add "cpe" netns "${NS}_net" type veth peer name "net" netns "${NS}_cpe"
+	ip link add "net" netns "${NS}_srv" type veth peer name "srv" netns "${NS}_net"
+
+	ip -n "${NS}_cli" link set "cpe" up
+	ip -n "${NS}_cli" addr add dev "cpe" 10.0.0.2/24
+	ip -n "${NS}_cli" route add default via 10.0.0.1 dev "cpe"
+
+	ip -n "${NS}_cpe" link set "cli" up
+	ip -n "${NS}_cpe" addr add dev "cli" 10.0.0.1/24
+	ip -n "${NS}_cpe" link set "net" up
+	ip -n "${NS}_cpe" addr add dev "net" 10.0.1.2/24
+	ip -n "${NS}_cpe" route add default via 10.0.1.1 dev "net"
+	tc -n "${NS}_cpe" qdisc add dev "net" root netem rate 10mbit delay 5ms
+
+	ip -n "${NS}_net" link set "cpe" up
+	ip -n "${NS}_net" addr add dev "cpe" 10.0.1.1/24
+	ip -n "${NS}_net" link set "srv" up
+	ip -n "${NS}_net" addr add dev "srv" 10.0.2.1/24
+	ip -n "${NS}_net" route add default via 10.0.1.2 dev "cpe"
+	tc -n "${NS}_net" qdisc add dev "cpe" root netem rate 10mbit delay 5ms
+
+	ip -n "${NS}_srv" link set "net" up
+	ip -n "${NS}_srv" addr add dev "net" 10.0.2.2/24
+	ip -n "${NS}_srv" route add default via 10.0.2.1 dev "net"
+}
+
+setup
+server
+
+tc_client
+tc_server
+
+case "${1}" in
+	*)
+		export -f tc_client tc_server
+		echo -e "\n\tNetns: $(netns)\n\tUse 'ip netns exec <NETNS> <CMD>' to execute a command in the netns.\n\tServer: iperf3 -c 10.0.2.2 -R\n"
+		ip netns exec ${NS}_cli bash
+		;;
+esac
