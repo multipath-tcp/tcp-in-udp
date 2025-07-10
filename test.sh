c28fe6bc30f34e8572903c343c014494139ed4a3
Matthieu Baerts <matttbe@kernel.org>
2025-07-10 15:32:07 +0200

    test: disable gro/gso on client/server
    We cannot just set gso_max_segs on the hosts doing the modification,
    because in this particular setup, they will receive aggregated packets,
    and are not the initiator of the original packets.
    
    Instead, we could set gso_max_segs on the client and server, but it will
    slightly affect the perf in this test lab.
    
    Signed-off-by: Matthieu Baerts <matttbe@kernel.org>

--- test.sh
+++ test.sh
@@ -50,7 +50,8 @@ tc_client()
 	tc -n "${ns}" filter show dev "${iface}" ingress
 
 	ip netns exec "${ns}" ethtool -K "${iface}" gro off gso off tso off lro off ufo off sg off
-	ip -n "${ns}" link set ${iface} gso_max_segs 1
+	# ip -n "${NS}_cli" link set "cpe" gso_max_segs 1 # but perf impact in this particular setup
+	ip netns exec "${NS}_cli" ethtool -K "cpe" gro off gso off tso off lro off ufo off sg off
 }
 
 tc_server()
@@ -68,7 +69,8 @@ tc_server()
 	tc -n "${ns}" filter show dev "${iface}" ingress
 
 	ip netns exec "${ns}" ethtool -K "${iface}" gro off gso off tso off lro off ufo off sg off
-	ip -n "${ns}" link set ${iface} gso_max_segs 1
+	# ip -n "${NS}_srv" link set "net" gso_max_segs 1 # but perf impact in this particular setup
+	ip netns exec "${NS}_srv" ethtool -K "net" gro off gso off tso off lro off ufo off sg off
 }
 
 capture()
