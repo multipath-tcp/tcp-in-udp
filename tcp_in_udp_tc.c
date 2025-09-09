multipath-tcp/tcp-in-udp

commit 58342d35a6f1c3594e4566fef7923a88d9041ed1

tc: support layer 3 interfaces
Cellular interfaces do not include layer 2 header. When reading the Ethernet
header, if there is no IPv4 or IPv6 header found, assume that the packet does
not have an Ethernet header and check whether the protocol is IPv4 or IPv6.

Signed-off-by: Chester A. Unal <chester.a.unal@arinc9.com>

tcp_in_udp_tc.c
@@ -335,7 +335,13 @@ int tc_tcp_in_udp(struct __sk_buff *skb)
 	} else if (eth_type == bpf_htons(ETH_P_IPV6)) {
 		ip_type = parse_ip6hdr(&nh, data_end, &ipv6hdr);
 	} else {
-		goto out;
+		nh.pos = data;
+		if (skb->protocol == bpf_htons(ETH_P_IP))
+			ip_type = parse_iphdr(&nh, data_end, &iphdr);
+		else if (skb->protocol == bpf_htons(ETH_P_IPV6))
+			ip_type = parse_ip6hdr(&nh, data_end, &ipv6hdr);
+		else
+			goto out;
 	}
 
 	if (ip_type == IPPROTO_TCP)

commit 0e8e857d8ea6dc298b820297da345f69335ccb1d

tc: fix misaligned packet access error thrown by BPF verifier
The ipv6hdr and iphdr variables in tc_tcp_in_udp() are modified multiple
times. The verifier doesn't seem to understand that one of these two
modifications is under an if block and the other is under the affine else
block so it rejects running the programme due to misaligned packet access
when attempting to load the programme on certain interfaces such as a DSA
user interface. Declare new variables for the else block to satisfy the
verifier.

Fixes: 58342d35a6f1 ("tc: support layer 3 interfaces")
Signed-off-by: Chester A. Unal <chester.a.unal@arinc9.com>

tcp_in_udp_tc.c
@@ -331,13 +331,24 @@ int tc_tcp_in_udp(struct __sk_buff *skb)
 	} else if (eth_type == bpf_htons(ETH_P_IPV6)) {
 		ip_type = parse_ip6hdr(&nh, data_end, &ipv6hdr);
 	} else {
+		struct ipv6hdr *ipv6hdr_alt = NULL;
+		struct iphdr *iphdr_alt = NULL;
+
 		nh.pos = data;
+
 		if (skb->protocol == bpf_htons(ETH_P_IP))
-			ip_type = parse_iphdr(&nh, data_end, &iphdr);
+			ip_type = parse_iphdr(&nh, data_end, &iphdr_alt);
 		else if (skb->protocol == bpf_htons(ETH_P_IPV6))
-			ip_type = parse_ip6hdr(&nh, data_end, &ipv6hdr);
+			ip_type = parse_ip6hdr(&nh, data_end, &ipv6hdr_alt);
 		else
 			goto out;
+
+		if (ip_type == IPPROTO_TCP)
+			return tcp_to_udp(skb, &nh, iphdr_alt, ipv6hdr_alt);
+		if (ip_type == IPPROTO_UDP)
+			udp_to_tcp(skb, &nh, iphdr_alt, ipv6hdr_alt);
+out:
+		return ret;
 	}
 
 	if (ip_type == IPPROTO_TCP)
@@ -345,7 +356,6 @@ int tc_tcp_in_udp(struct __sk_buff *skb)
 	if (ip_type == IPPROTO_UDP)
 		udp_to_tcp(skb, &nh, iphdr, ipv6hdr);
 
-out:
 	return ret;
 }
 