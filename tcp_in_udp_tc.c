multipath-tcp/tcp-in-udp

commit 58342d35a6f1c3594e4566fef7923a88d9041ed1

tc: support layer 3 interfaces
Cellular interfaces do not include layer 2 header. When reading the Ethernet
header, if there is no IPv4 or IPv6 header found, assume that the packet does
not have an Ethernet header and check whether the protocol is IPv4 or IPv6.

Signed-off-by: Chester A. Unal <chester.a.unal@arinc9.com>

tcp_in_udp_tc.c
@@ -335,7 +335,13 @@ int tc_tcp_in_udp(struct __sk_buff *skb)
 	} else if (eth_type == bpf_htons(ETH_P_IPV6)) {
 		ip_type = parse_ip6hdr(&nh, data_end, &ipv6hdr);
 	} else {
-		goto out;
+		nh.pos = data;
+		if (skb->protocol == bpf_htons(ETH_P_IP))
+			ip_type = parse_iphdr(&nh, data_end, &iphdr);
+		else if (skb->protocol == bpf_htons(ETH_P_IPV6))
+			ip_type = parse_ip6hdr(&nh, data_end, &ipv6hdr);
+		else
+			goto out;
 	}
 
 	if (ip_type == IPPROTO_TCP)

commit 271a52a6f7198036cc8466708969c04a9a0f0df6

{README,tc}: fix misaligned packet access error thrown by BPF verifier
The BPF verifier rejects running the programme due to misaligned packet
access when attempting to load the programme on certain interfaces such as
a DSA user interface. Split the layer 3 interface support out to a separate
ELF section to address this.

Fixes: 58342d35a6f1 ("tc: support layer 3 interfaces")
Signed-off-by: Chester A. Unal <chester.a.unal@arinc9.com>

tcp_in_udp_tc.c
@@ -315,7 +315,7 @@ out:
 }
 
 SEC("tc")
-int tc_tcp_in_udp(struct __sk_buff *skb)
+int tc_tcp_in_udp_l2(struct __sk_buff *skb)
 {
 	void *data_end = (void *)(long)skb->data_end;
 	void *data = (void *)(long)skb->data;
@@ -326,19 +326,38 @@ int tc_tcp_in_udp(struct __sk_buff *skb)
 	struct ethhdr *eth;
 
 	eth_type = parse_ethhdr(&nh, data_end, &eth);
-	if (eth_type == bpf_htons(ETH_P_IP)) {
+	if (eth_type == bpf_htons(ETH_P_IP))
 		ip_type = parse_iphdr(&nh, data_end, &iphdr);
-	} else if (eth_type == bpf_htons(ETH_P_IPV6)) {
+	else if (eth_type == bpf_htons(ETH_P_IPV6))
 		ip_type = parse_ip6hdr(&nh, data_end, &ipv6hdr);
-	} else {
-		nh.pos = data;
-		if (skb->protocol == bpf_htons(ETH_P_IP))
-			ip_type = parse_iphdr(&nh, data_end, &iphdr);
-		else if (skb->protocol == bpf_htons(ETH_P_IPV6))
-			ip_type = parse_ip6hdr(&nh, data_end, &ipv6hdr);
-		else
-			goto out;
-	}
+	else
+		goto out;
+
+	if (ip_type == IPPROTO_TCP)
+		return tcp_to_udp(skb, &nh, iphdr, ipv6hdr);
+	if (ip_type == IPPROTO_UDP)
+		udp_to_tcp(skb, &nh, iphdr, ipv6hdr);
+
+out:
+	return ret;
+}
+
+SEC("tc_l3")
+int tc_tcp_in_udp_l3(struct __sk_buff *skb)
+{
+	void *data_end = (void *)(long)skb->data_end;
+	void *data = (void *)(long)skb->data;
+	struct hdr_cursor nh = { .pos = data };
+	int ip_type, ret = TC_ACT_OK;
+	struct ipv6hdr *ipv6hdr = NULL;
+	struct iphdr *iphdr = NULL;
+
+	if (skb->protocol == bpf_htons(ETH_P_IP))
+		ip_type = parse_iphdr(&nh, data_end, &iphdr);
+	else if (skb->protocol == bpf_htons(ETH_P_IPV6))
+		ip_type = parse_ip6hdr(&nh, data_end, &ipv6hdr);
+	else
+		goto out;
 
 	if (ip_type == IPPROTO_TCP)
 		return tcp_to_udp(skb, &nh, iphdr, ipv6hdr);